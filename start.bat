5788c0b843c7d9b229dec05b7e970e8cde06ade6
Adalberto-Junior <86660513+Adalberto-Junior@users.noreply.github.com>
2025-04-23 17:18:18 +0100

    refactor: simplify start.bat by removing production check for React

--- start.bat
+++ start.bat
@@ -17,15 +17,8 @@ start "Backend" cmd /k "python app.py"
 cd ../frontend
 echo 🔄 Iniciando frontend React...
 
-:: Verifica se existe a pasta build (produção) ou roda o React dev server
-IF EXIST "build" (
-    echo 🧱 Modo PRODUÇÃO - React será servido pelo Flask.
-    timeout /t 2
-    start "" http://localhost:5000
-) ELSE (
-    echo ⚛️ Modo DEV - Iniciando React dev server...
-    start "frontend" cmd /k "npm run dev"
-)
+ cmd /k "npm run dev"
+
 
 :: Voltar à raiz
 cd ..
